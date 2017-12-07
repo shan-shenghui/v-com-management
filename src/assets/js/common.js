6c6c7c3ed96247803f3de692d2eeee09cc74ea8f
sshcx <1275526416@qq.com>
2017-12-07 18:28:34 +0800

    change somesthing

---
+++ src/assets/js/common.js
@@ -0,0 +1,12 @@
+export function debounce(fn, delay) {  //函数去抖
+  var timer = null;
+  return function () {
+    var context = this, args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
+  };
+}
+
+
--- src/components/system/company/company.vue
+++ src/components/system/company/company.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="company">
-    <!-- 为ECharts准备一个具备大小（宽高）的Dom 。 动态初始化多个echarts -->
     <draggable>
       <transition-group>
-        <div v-for="item in myArray" :key="item" class="eDom" :id="item">
+        <div v-for="item in eArray" :key="item" class="eDom" :id="item">
         </div>
       </transition-group>
     </draggable>
@@ -19,19 +18,19 @@
   import echarts from 'echarts'
   import draggable from 'vuedraggable'
 
-
   export  default {
     data (){
       return {
         optionDatas: [],
-        myArray: []
+        eArray: [],
+        screenWidth:0
       }
     },
     mounted (){
-      this.initEcharts();
+      this.getEchartsData();
     },
     methods: {
-      initEcharts (){
+      getEchartsData (){
         const _this = this;
         this.$http.get('http://localhost:8080/static/echarts.json').then(function (response) {
           if (response.data.length) {
@@ -39,8 +38,7 @@
 
             //初始化多个dom
             _this.optionDatas.forEach(function (item, index) {
-              _this.myArray.push(item.id);  //e_0 e_1 ...
-
+              _this.eArray.push(item.id);
             })
 
           } else {
@@ -48,20 +46,23 @@
           }
         }).then(function () {
 
-          //dom初始化后 初始化echarts
-          if (_this.optionDatas.length) {
-            _this.optionDatas.forEach(function (item) {
-              var dom = document.getElementById(item.id);
-              var myChart = echarts.init(dom);
-              myChart.setOption(item.option);
-            })
-          } else {
-            console.log('数据返回为空');
-          }
+          _this.initEcharts(); //初始化echarts
 
         }).catch(function (error) {
           console.log(error);
-        })
+        });
+
+      },
+      initEcharts(){
+        if (this.optionDatas.length) {
+          this.optionDatas.forEach(function (item) {
+            var dom = document.getElementById(item.id);
+            var myChart = echarts.init(dom);
+            myChart.setOption(item.option);
+          })
+        } else {
+          console.log('数据返回为空');
+        }
       },
 
     },
